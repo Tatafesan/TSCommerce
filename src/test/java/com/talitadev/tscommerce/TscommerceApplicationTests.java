aa736243f9957dae3b85d67aacb51a77ddd55ae9
Tatafesan <talitafesan@gmail.com>
2022-12-20 01:11:56 -0400

    Order and User

---
+++ src/test/java/com/talitadev/tscommerce/TscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talitadev.tscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
