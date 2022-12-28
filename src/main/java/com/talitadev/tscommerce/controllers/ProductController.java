d4ff6db1b4ffd6fb631e9fcd3cdad93d7208d6a0
Tatafesan <talitafesan@gmail.com>
2022-12-28 11:10:00 -0400

    Insert new product POST

--- src/main/java/com/talitadev/tscommerce/controllers/ProductController.java
+++ src/main/java/com/talitadev/tscommerce/controllers/ProductController.java
@@ -1,16 +1,11 @@
 package com.talitadev.tscommerce.controllers;
 
 import com.talitadev.tscommerce.dto.ProductDTO;
-import com.talitadev.tscommerce.entities.Product;
-import com.talitadev.tscommerce.repositories.ProductRepository;
 import com.talitadev.tscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +23,10 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto){
+        return service.insert(dto);
     }
 }
