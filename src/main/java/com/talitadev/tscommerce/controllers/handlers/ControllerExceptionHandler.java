292a35e1193788bc503f2c9f15517a234a3e9f45
Tatafesan <talitafesan@gmail.com>
2023-01-08 15:18:17 -0400

    Bean Validation

--- src/main/java/com/talitadev/tscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/talitadev/tscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.talitadev.tscommerce.controllers.handlers;
 
 import com.talitadev.tscommerce.dto.CustomError;
+import com.talitadev.tscommerce.dto.ValidationError;
 import com.talitadev.tscommerce.services.exceptions.DatabaseException;
 import com.talitadev.tscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
